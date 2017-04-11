c008744396fe75861fd0acc1c2f774d3547e7c77
Fan Yang <snisiarc@gmail.com>
2017-04-11 12:41:13 -0500

    modified the initial processing with creating folders in R

---
+++ 1_preprocessing.md
@@ -0,0 +1,17 @@
+# Preprocessing community data
+
+## Objectives:  
+1. Create phyloseq objects using raw OTU table and taxonomy table.   
+2. Eliminate ambiguous rare OTUs that summed up to less than 5 across all samples.  
+3. Generate a histogram to evaluate sample sequencing depths.   
+4. Calculate the Good's Estimate of Coverage for each sample. 
+5. Generate a histogram to evaluate sample coverage from sequencing.   
+6. Save any output that could be used downstream directly.  
+
+###1. This step does all of the above and generates 3 different directories. In terminal, type:   
+```
+cd ~/pit_foaming_manuscript  
+Rscript codes/R_scripts/community_sample_processing_phyloseq.R raw_data/otu_table.txt raw_data/taxa_table.txt
+```
+
+
--- codes/R_scripts/community_sample_processing_phyloseq.R
+++ codes/R_scripts/community_sample_processing_phyloseq.R
@@ -15,6 +15,11 @@
 library(phyloseq)
 library(vegan)
 
+## create three directories in current location to store different types of outputs:
+dir.create("RDS_objects")
+dir.create("Figures")
+dir.create("Text_outputs")
+
 args <- commandArgs(TRUE)
 ## input arguments:
 ### 1: otu table with taxa in rows
@@ -25,29 +30,27 @@ otu <- read.delim(args[1], row.names = 1) #read in otu table
 print("the dimension of the OTU table is: ")
 dim(otu)
 
-tax <- read.delim(args[2]) #read in taxa table
+tax <- read.delim(args[2], row.names=1) #read in taxa table
 # check taxa table dimension
 print("the dimension of the taxa table is: ")
 dim(tax)
-
-row.names(tax)<-tax$OTUS
 
 data.phy<-phyloseq(otu_table(as.matrix(otu),taxa_are_rows=T), tax_table(as.matrix(tax)))
 # check phyloseq object size
 print("the phyloseq object contains: ")
 data.phy
 print("saving raw sequence phyloseq object to current directory ... ")
-saveRDS(data.phy, "raw_sequence_phyloseq.RDS")
+saveRDS(data.phy, "RDS_objects/raw_sequence_phyloseq.RDS")
 
 ## get rid of any taxa that summing up to be less than 5 across all samples
 data.taxmin5.phy<-prune_taxa(taxa_sums(data.phy) >= 5, data.phy)
 data.taxmin5.phy<-prune_samples(sample_sums(data.taxmin5.phy) > 0, data.taxmin5.phy) #get rid of samples with all 0's, if it exists
 print("saving phyloseq object with taxa sum >= 5 across all samples to current directory ... ")
-saveRDS(data.taxmin5.phy, "taxsum_min5_sequence_phyloseq.RDS")
+saveRDS(data.taxmin5.phy, "RDS_objects/taxsum_min5_sequence_phyloseq.RDS")
 
 ## save the histogram of sample sequencing depth distribution
 print("Generating histogram on sample sequencing depth to current directory ... ")
-pdf("data.taxmin5.sequencing_depth_hist.pdf")
+pdf("Figures/data.taxmin5.sequencing_depth_hist.pdf")
 hist(sample_sums(data.taxmin5.phy), breaks = nsamples(data.taxmin5.phy)/2)
 dev.off()
 
@@ -61,11 +64,11 @@ si$C <- 1-(si$n1 / si$sample_sums) #calculate Good's coverage
 si$SAMPLES <- row.names(si)
 ## save the sample coverage information to file
 print("Saving the sample coverage information as a text file in current directory ... ")
-write.table(si, "data.taxmin5.sample_goods_coverage.txt", sep = "\t", quote = F, row.names = F)
+write.table(si, "text_outputs/data.taxmin5.sample_goods_coverage.txt", sep = "\t", quote = F, row.names = F)
 
 ## plot Good's coverage histogram
 print("Generating histogram on Good's estimated coverage to current directory ... ")
-pdf("data.taxmin5.goods_coverage_hist.pdf")
+pdf("Figures/data.taxmin5.goods_coverage_hist.pdf")
 hist(si$C, breaks = nrow(si)/2)
 dev.off()
 
